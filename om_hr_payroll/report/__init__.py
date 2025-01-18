78fbe8a64384d26dec1f59c0c7f665552b912625
Naeem Pasha <naeem@greensfin.com>
2025-01-18 05:56:38 +0000

    custome payroll module with urdu support

---
+++ om_hr_payroll/report/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding:utf-8 -*-
+
+from . import report_payslip_details
+from . import report_payslip_details_urdu
+from . import report_payslip_urdu
+from . import report_contribution_register
